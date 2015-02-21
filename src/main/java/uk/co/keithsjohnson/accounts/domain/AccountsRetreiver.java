694cee841d127202da8db4073c777645cc9363d1
keithsjohnson <keithsjohnson@yahoo.com>
2015-02-21 15:57:28 +0000

    Add reading application.properties and displaying on index.html.

--- src/main/java/uk/co/keithsjohnson/accounts/domain/AccountsRetreiver.java
+++ src/main/java/uk/co/keithsjohnson/accounts/domain/AccountsRetreiver.java
@@ -14,6 +14,8 @@ public class AccountsRetreiver {
 		List<Account> accounts = new ArrayList<>();
 		Account account = new Account("Current", 0);
 		accounts.add(account);
+		Account account2 = new Account("Savings", 10);
+		accounts.add(account2);
 		return accounts;
 	}
 
--- src/main/java/uk/co/keithsjohnson/accounts/view/home/HomeView.java
+++ src/main/java/uk/co/keithsjohnson/accounts/view/home/HomeView.java
@@ -1,6 +1,7 @@
 package uk.co.keithsjohnson.accounts.view.home;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,18 @@ import uk.co.keithsjohnson.accounts.action.GetAccountsAction;
 
 @Controller
 public class HomeView {
+
+	@Value("${test.value}")
+	String testValue;
+
+	@Value("${value1}")
+	String value1;
+
+	@Value("${value2}")
+	String value2;
+
+	@Value("${values}")
+	String[] values;
 
 	private final GetAccountsAction getAccountsAction;
 
@@ -20,6 +33,11 @@ public class HomeView {
 	@RequestMapping(value = "/")
 	public String index(Model model) {
 		model.addAttribute("accounts", getAccountsAction.getAccounts());
+		model.addAttribute("value1", value1);
+		model.addAttribute("value2", value2);
+		model.addAttribute("testValue", testValue);
+		model.addAttribute("values", values);
+		System.out.println("Model: " + model.toString());
 		return "index";
 	}
 }
